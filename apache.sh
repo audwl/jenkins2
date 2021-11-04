4d7d049dbcd477299eb50e8ab8fd293800f672e2
audwl <audwl44@naver.com>
2021-11-04 16:21:53 +0900

    babo

--- apache.sh
+++ apache.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-docker pull httpd
-docker run -itd -p 60888:80 --name h1 httpd
+sudo yum install -y httpd
+cat > /var/www/html/index.html << END
+<html><body><h1>apache-aws-jenkins-github</h1></body></html>
+END
+systemctl start httpd
 echo "byebye"
